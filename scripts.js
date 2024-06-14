ca1e8cf53ccfe98cee9a955d1620766c1572e1c9
guigaldino <128167519+guigaldino@users.noreply.github.com>
2024-06-13 22:46:12 -0300

    correção API

--- scripts.js
+++ scripts.js
@@ -1,38 +1,47 @@
-//VARIAVEIS
-let campoPesquisar = $("#inputPesquisar")
-let botaoPesquisar = $("#buttonPesquisar")
-let cardResultado = $("#cardResultado")
-let infoFilmes = $("#infoFilmes")
-let disponibilidadeStreaming = $("#disponibilidadeStreaming")
+// VARIÁVEIS
+let campoPesquisar = $("#inputPesquisar");
+let botaoPesquisar = $("#buttonPesquisar");
+let cardResultado = $("#cardResultado");
+let infoFilmes = $("#infoFilmes");
+let disponibilidadeStreaming = $("#disponibilidadeStreaming");
 
-//EVENTOS
+// EVENTOS
 botaoPesquisar.click(function () {
     if (campoPesquisar.val() === '') {
-        alert("digite algo")
+        alert("Digite algo");
     } else {
-        pesquisar()
+        pesquisar();
     }
-})
+});
 
-//FUNÇÕES
+// FUNÇÕES
 async function pesquisar() {
-    let filmePesquisado = campoPesquisar.val()
-    alert(filmePesquisado)
-    let pesquisarFilme = await apiPesquisarFilme()
-    console.log(pesquisarFilme)
+    let filmePesquisado = campoPesquisar.val();
+    let pesquisarFilme = await apiPesquisarFilme(filmePesquisado)
+    inserirInformacoes(pesquisarFilme)
 }
 
-async function apiPesquisarFilme() {
+function inserirInformacoes(array){
+    let imgUrl = array.Poster
+    imgUrl = imgUrl.replace(/^["'](.+(?=["']$))["']$/, '$1')
+    console.log(imgUrl)
+    let poster = $("<img>").attr('src', imgUrl)
+    infoFilmes.append(poster)
+}
+
+async function apiPesquisarFilme(titulo) {
     var raw = "";
 
     var requestOptions = {
         method: 'GET',
-        body: raw,
         redirect: 'follow'
     };
 
-    fetch("http://www.omdbapi.com/?apikey=85cb41f0&t=titanic", requestOptions)
-        .then(response => response.text())
-        .then(result => alert(result))
-        .catch(error => alert('error', error));
+    try {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=85cb41f0&t=${titulo}`, requestOptions);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        alert('error', error);
+    }
 }
